b63c893dfa3f74a5d577c2a100e1ccc02fac4629
thangSu <97284217+thangSu@users.noreply.github.com>
2022-05-30 22:31:10 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,10 @@ resource "aws_iam_user_policy_attachment" "attachment" {
   user       = aws_iam_user.iu.name
   policy_arn = aws_iam_policy.ip.arn
 }
-
+resource "aws_iam_role" "example" {
+  name                = "UpdateApp"
+  managed_policy_arns = [aws_iam_policy.ip.arn]
+}
 resource "aws_iam_policy" "ip" {
     name = "accessread"
     path = "/"
@@ -26,7 +29,7 @@ resource "aws_iam_policy" "ip" {
             "s3:ListBucket",
             "s3:GetBucketLocation"
         ],
-        "Resource": "arn:aws:s3:::productionapp"
+        "Resource": "arn:aws:s3:::thangdeptrai"
         },
         {
         "Effect": "Allow",
@@ -35,7 +38,7 @@ resource "aws_iam_policy" "ip" {
             "s3:PutObject",
             "s3:DeleteObject"
         ],
-        "Resource": "arn:aws:s3:::productionapp/*"
+        "Resource": "arn:aws:s3:::thangdeptrai/*"
         }
     ]
     }
@@ -45,4 +48,19 @@ resource "aws_iam_policy" "ip" {
 resource "aws_iam_user" "iu" {
     name = "thangaccess"
     path = "/system/"
+}
+resource "aws_iam_user_policy" "lb_ro" {
+  name = "test"
+  user = aws_iam_user.lb.name
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": {
+      "Effect": "Allow",
+      "Action": "sts:AssumeRole",
+      "Resource": "arn:aws:iam::704063666843:role/UpdateApp"
+    }
+  }
+EOF
 }
