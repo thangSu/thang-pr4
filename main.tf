34e7519e803ff87f0985eb72418125eb74920a61
thangSu <phamthang3003@gmail.com>
2022-06-06 12:30:18 +0700

    1206

--- main.tf
+++ main.tf
@@ -1,16 +1,41 @@
 provider "aws" {
   region = "us-east-1"
 }
+variable "bucket_name" {
+  type = string
+  default = "hcl-demo-test"
+}
+
 locals {
   map= {
-    ip_dynamic= {
-       "id1"={
+    princilpals_dynamic = {
+    "1"={
       type = "AWS"
       identifiers = ["704063666843"],
     }
-     "id2"={
+    "2"={
       type = "AWS"
       identifiers = ["878103297030"],
+    }
+    statement_dynamic={
+    "1" ={
+       actions = ["s3:ListAllMyBuckets",]
+       Effect = "Allow"
+       resources = ["*",]
+    }
+    "2" ={
+       actions = [ "s3:ListBucket",
+                "s3:GetBucketLocation",]
+        Effect = "Allow"
+       resources = ["arn:aws:s3:::${var.bucket_name}",]
+    }
+    "3" ={
+       actions = [  "s3:GetObject",
+                "s3:PutObject",
+                "s3:DeleteObject",]
+        Effect = "Allow"
+       resources = ["arn:aws:s3:::${var.bucket_name}/*",]
+    }
     }
     }
   }
@@ -21,7 +46,7 @@ data "aws_iam_policy_document" "ip_document" {
     actions = ["sts:AssumeRole"]
 
     dynamic "principals" {
-      for_each = local.map.ip_dynamic
+      for_each = local.map.princilpals_dynamic
       content {
         type        = principals.value.type
         identifiers = principals.value.identifiers
@@ -29,52 +54,34 @@ data "aws_iam_policy_document" "ip_document" {
     }
   }
 }
-
-resource "aws_iam_role" "example" {
-  name                = "UpdateApp"
+resource "aws_iam_role" "role" {
+  name                = "ThangUpdateApp"
   assume_role_policy = data.aws_iam_policy_document.ip_document.json
   managed_policy_arns = [aws_iam_policy.ip.arn]
 }
 
+data "aws_iam_policy_document" "ip_document2" {
 
+  dynamic "statement" {
+    for_each = local.map.statement_dynamic
+
+    content{
+      actions = statement.value.actions
+      Effect = statement.value.Effect
+      resources = statement.value.resources
+    }
+  }
+}
 resource "aws_iam_policy" "ip" {
-    name = "accessread"
+    name = "AccessRead"
     path = "/"
     description = "access to S3 bucket"
 
-    policy = jsonencode(
-    {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-        "Effect": "Allow",
-        "Action": "s3:ListAllMyBuckets",
-        "Resource": "*"
-        },
-        {
-        "Effect": "Allow",
-        "Action": [
-            "s3:ListBucket",
-            "s3:GetBucketLocation"
-        ],
-        "Resource": "arn:aws:s3:::thangdeptrai"
-        },
-        {
-        "Effect": "Allow",
-        "Action": [
-            "s3:GetObject",
-            "s3:PutObject",
-            "s3:DeleteObject"
-        ],
-        "Resource": "arn:aws:s3:::thangdeptrai/*"
-        }
-    ]
-    }
-    )
+    policy = data.aws_iam_policy_document.ip_document2
 }
 
 resource "aws_iam_user" "iu" {
-    name = "thangaccess"
+    name = "ThangAccess"
     path = "/system/"
 }
 resource "aws_iam_user_policy" "lb_ro" {
@@ -87,7 +94,7 @@ resource "aws_iam_user_policy" "lb_ro" {
     "Statement": {
       "Effect": "Allow",
       "Action": "sts:AssumeRole",
-      "Resource": "arn:aws:iam::704063666843:role/UpdateApp"
+      "Resource": "arn:aws:iam::704063666843:role/ThangUpdateApp"
     }
   }
 EOF
